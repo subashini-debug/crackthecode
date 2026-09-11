2813c14d5d847f66797b4079a7463853f5a871b3
subashini-debug <subashinikarunakaran27@gmail.com>
2026-09-11 08:57:33 +0530

    Auto-create data directory before lowdb init

--- server/db.js
+++ server/db.js
@@ -1,8 +1,11 @@
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const path = require('path');
+const fs = require('fs');
 
-const adapter = new FileSync(path.join(__dirname, 'data', 'db.json'));
+const dataDir = path.join(__dirname, 'data');
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
+
+const adapter = new FileSync(path.join(dataDir, 'db.json'));
 const db = low(adapter);
 
 // Default schema
