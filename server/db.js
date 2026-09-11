70b76ddf372bece4b6ded578445fb2eab8f4c78f
subashini-debug <subashinikarunakaran27@gmail.com>
2026-09-11 09:02:18 +0530

    Add missing require for path, lowdb, FileSync in db.js

--- server/db.js
+++ server/db.js
@@ -1,3 +1,6 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const path = require('path');
 const fs = require('fs');
 
 const dataDir = path.join(__dirname, 'data');
